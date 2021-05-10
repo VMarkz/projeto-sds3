66daa11bdf4407025a018a3da9ca584014daae24
VMarkz <vitor.portela@bandtec.com.br>
2021-05-10 10:20:34 -0300

    Update Footer

--- frontend/src/componentes/Footer/index.tsx
+++ frontend/src/componentes/Footer/index.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Vitor Marques</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/VMarkz" target="_blank" rel="noreferrer">Vitor Marques</a></p>
+                <p className="text-light"><small><strong>Desenvolvido utilizando Spring Boot e React</strong><br />
+      Meu <a href="https://www.linkedin.com/in/vitor-marques-73b8991a9/" target="_blank" rel="noreferrer">Linkedin</a></small></p>
             </div>
         </footer>
     );
